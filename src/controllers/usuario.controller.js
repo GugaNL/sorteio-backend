41d854d85609ba18ab8775fad602c7bdd81b3e44
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2022-09-15 13:51:28 -0300

    teste

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,4 +1,4 @@
-// const usuarioService = require("../services/usuario.service");
+const usuarioService = require("../services/usuario.service");
 const { validationResult } = require("express-validator");
 const createError = require("http-errors");
 
@@ -10,37 +10,17 @@ const create = async function (req, res, next) {
       throw createError(422, { errors: errors.array() });
     }
 
-    // const response = await usuarioService.create(req.body);
+    const response = await usuarioService.create(req.body);
 
-    // if (response && response.message) {
-    //   throw response;
-    // }
+    if (response && response.message) {
+      throw response;
+    }
 
-    // res.send(response);
+    res.send(response);
   } catch (error) {
     return next(error);
   }
 };
-
-// const create = async function (req, res, next) {
-//   try {
-//     const errors = validationResult(req);
-
-//     if (!errors.isEmpty()) {
-//       throw createError(422, { errors: errors.array() });
-//     }
-
-//     const response = await usuarioService.create(req.body);
-
-//     if (response && response.message) {
-//       throw response;
-//     }
-
-//     res.send(response);
-//   } catch (error) {
-//     return next(error);
-//   }
-// };
 
 // const login = async function (req, res, next) {
 //   try {
--- src/services/usuario.service.js
+++ src/services/usuario.service.js
@@ -1,101 +1,105 @@
-const usuarioRepository = require("../repositories/usuario.repository");
+//const usuarioRepository = require("../repositories/usuario.repository");
 require("dotenv").config();
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
 const { sign } = require("jsonwebtoken");
 
 const create = async function (usuario) {
-  const userExists = await usuarioRepository.findUserToValidate({
-    login: usuario.login,
-  });
-
-  if (userExists) {
-    return createError(409, "Usuário já existe");
-  }
-
-  usuario.senha = await bcrypt.hash(usuario.senha, ~~process.env.SALT);
-  const usuarioCriado = await usuarioRepository.create(usuario);
-  return usuarioCriado;
-};
-
-const listUsers = async function () {
-  const usuarios = await usuarioRepository.listUsers();
-  return usuarios;
-};
-
-const findUser = async function (id) {
-  const usuario = await usuarioRepository.findUser(id);
-
-  if (!usuario) {
-    return createError(404, "Usuário não encontrado");
-  }
-
   return usuario;
 };
 
-const updateUser = async function (usuario, id) {
-  const userExists = await usuarioRepository.findUser(id);
+// const create = async function (usuario) {
+//   const userExists = await usuarioRepository.findUserToValidate({
+//     login: usuario.login,
+//   });
 
-  if (!userExists) {
-    return createError(404, "Usuário não existe");
-  }
+//   if (userExists) {
+//     return createError(409, "Usuário já existe");
+//   }
 
-  await usuarioRepository.updateUser(usuario, id);
+//   usuario.senha = await bcrypt.hash(usuario.senha, ~~process.env.SALT);
+//   const usuarioCriado = await usuarioRepository.create(usuario);
+//   return usuarioCriado;
+// };
 
-  return await usuarioRepository.findUser(id);
-};
+// const listUsers = async function () {
+//   const usuarios = await usuarioRepository.listUsers();
+//   return usuarios;
+// };
 
-const deleteUser = async function (id) {
-  const usuario = await usuarioRepository.findUser(id);
+// const findUser = async function (id) {
+//   const usuario = await usuarioRepository.findUser(id);
 
-  if (!usuario) {
-    return createError(404, "Usuário não encontrado");
-  }
+//   if (!usuario) {
+//     return createError(404, "Usuário não encontrado");
+//   }
 
-  await usuarioRepository.deleteUser(id);
+//   return usuario;
+// };
 
-  return usuario;
-};
+// const updateUser = async function (usuario, id) {
+//   const userExists = await usuarioRepository.findUser(id);
 
-const login = async function (usuario) {
-  const userLogin = await usuarioRepository.findUserToValidate({
-    login: usuario.login,
-  });
+//   if (!userExists) {
+//     return createError(404, "Usuário não existe");
+//   }
 
-  if (!userLogin) {
-    return createError(401, "Usuário inválido");
-  }
+//   await usuarioRepository.updateUser(usuario, id);
 
-  const comparePassword = await bcrypt.compare(usuario.senha, userLogin.senha);
+//   return await usuarioRepository.findUser(id);
+// };
 
-  if (!comparePassword) {
-    return createError(401, "Usuário inválido");
-  }
+// const deleteUser = async function (id) {
+//   const usuario = await usuarioRepository.findUser(id);
 
-  const token = sign(
-    {
-      id: userLogin.id,
-    },
-    process.env.SECRET,
-    {
-      expiresIn: "5h",
-    }
-  );
+//   if (!usuario) {
+//     return createError(404, "Usuário não encontrado");
+//   }
 
-  delete userLogin.senha;
+//   await usuarioRepository.deleteUser(id);
 
-  return {
-    auth: true,
-    usuario: userLogin,
-    token
-  };
-};
+//   return usuario;
+// };
+
+// const login = async function (usuario) {
+//   const userLogin = await usuarioRepository.findUserToValidate({
+//     login: usuario.login,
+//   });
+
+//   if (!userLogin) {
+//     return createError(401, "Usuário inválido");
+//   }
+
+//   const comparePassword = await bcrypt.compare(usuario.senha, userLogin.senha);
+
+//   if (!comparePassword) {
+//     return createError(401, "Usuário inválido");
+//   }
+
+//   const token = sign(
+//     {
+//       id: userLogin.id,
+//     },
+//     process.env.SECRET,
+//     {
+//       expiresIn: "5h",
+//     }
+//   );
+
+//   delete userLogin.senha;
+
+//   return {
+//     auth: true,
+//     usuario: userLogin,
+//     token
+//   };
+// };
 
 module.exports = {
   create,
-  login,
-  listUsers,
-  findUser,
-  updateUser,
-  deleteUser,
+  // login,
+  // listUsers,
+  // findUser,
+  // updateUser,
+  // deleteUser,
 };
