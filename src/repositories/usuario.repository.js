d90ac5ed1e41c4fb38b956233535644f078940da
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2022-09-15 16:00:03 -0300

    teste

--- src/repositories/usuario.repository.js
+++ src/repositories/usuario.repository.js
@@ -5,41 +5,41 @@ const create = async function (usuario) {
   return usuarioCriado;
 };
 
-// const listUsers = async function () {
-//   const usuarios = await Usuario.findAll();
-//   return usuarios;
-// };
+const listUsers = async function () {
+  const usuarios = await Usuario.findAll();
+  return usuarios;
+};
 
-// const findUser = async function (id) {
-//   const usuario = await Usuario.findByPk(id);
-//   return usuario;
-// };
+const findUser = async function (id) {
+  const usuario = await Usuario.findByPk(id);
+  return usuario;
+};
 
-// const findUserToValidate = async function (where) {
-//   const usuario = await Usuario.findOne({
-//     where,
-//   });
+const findUserToValidate = async function (where) {
+  const usuario = await Usuario.findOne({
+    where,
+  });
 
-//   return usuario;
-// };
+  return usuario;
+};
 
-// const updateUser = async function (usuario, id) {
-//   await Usuario.update(usuario, {
-//     where: {
-//       id,
-//     },
-//   });
-// };
+const updateUser = async function (usuario, id) {
+  await Usuario.update(usuario, {
+    where: {
+      id,
+    },
+  });
+};
 
-// const deleteUser = async function (id) {
-//   return await Usuario.destroy({ where: { id } });
-// };
+const deleteUser = async function (id) {
+  return await Usuario.destroy({ where: { id } });
+};
 
 module.exports = {
   create,
-  // listUsers,
-  // findUser,
-  // findUserToValidate,
-  // updateUser,
-  // deleteUser
+  listUsers,
+  findUser,
+  findUserToValidate,
+  updateUser,
+  deleteUser
 };
