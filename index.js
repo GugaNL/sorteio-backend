14e671f8aa86177f00cbd20b1e363ee118798930
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2022-09-15 15:53:42 -0300

    teste

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
+require('dotenv').config();
 const express = require("express");
+const bodyPaser = require("body-parser");
 const app = express();
 
-const bodyPaser = require("body-parser");
-const port = process.env.PORT || 3000;
+const handle404Error = require('./src/middlewares/handle404Error');
 
-const handle404Error = require("./src/middlewares/handle404Error");
-const handleError = require("./src/middlewares/handleError");
-const usuarioRoute = require("./src/routers/usuario.route");
-const clienteRoute = require("./src/routers/cliente.route");
-const sorteioRoute = require("./src/routers/sorteio.route");
-const bilheteRoute = require("./src/routers/bilhete.route");
-
-app.get("/", function(req, res) {
-    res.send("Funcionou!")
-})
+const usuarioRoute = require('./src/routers/usuario.route');
+const clienteRoute = require('./src/routers/cliente.route');
+const sorteioRoute = require('./src/routers/sorteio.route');
+const bilheteRoute = require('./src/routers/bilhete.route');
+const handleError = require('./src/middlewares/handleError');
 
 app.use(bodyPaser.urlencoded({ extended: true }));
 app.use(bodyPaser.json());
-app.use(handle404Error);
-app.use(handleError);
 app.use('/api/usuario', usuarioRoute);
 app.use('/api/cliente', clienteRoute);
 app.use('/api/sorteio', sorteioRoute);
 app.use('/api/bilhete', bilheteRoute);
+app.use(handle404Error);
+app.use(handleError);
 
-app.listen(port, () => {
-  console.log("rodando na porta 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("rodando na porta 5000");
 });
