3c3c936e6a16378ea669b7a7692d7f4c29de553b
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2022-09-15 13:54:28 -0300

    teste

--- src/services/usuario.service.js
+++ src/services/usuario.service.js
@@ -1,26 +1,22 @@
-//const usuarioRepository = require("../repositories/usuario.repository");
+const usuarioRepository = require("../repositories/usuario.repository");
 require("dotenv").config();
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
 const { sign } = require("jsonwebtoken");
 
 const create = async function (usuario) {
-  return usuario;
+  const userExists = await usuarioRepository.findUserToValidate({
+    login: usuario.login,
+  });
+
+  if (userExists) {
+    return createError(409, "Usuário já existe");
+  }
+
+  usuario.senha = await bcrypt.hash(usuario.senha, ~~process.env.SALT);
+  const usuarioCriado = await usuarioRepository.create(usuario);
+  return usuarioCriado;
 };
-
-// const create = async function (usuario) {
-//   const userExists = await usuarioRepository.findUserToValidate({
-//     login: usuario.login,
-//   });
-
-//   if (userExists) {
-//     return createError(409, "Usuário já existe");
-//   }
-
-//   usuario.senha = await bcrypt.hash(usuario.senha, ~~process.env.SALT);
-//   const usuarioCriado = await usuarioRepository.create(usuario);
-//   return usuarioCriado;
-// };
 
 // const listUsers = async function () {
 //   const usuarios = await usuarioRepository.listUsers();
