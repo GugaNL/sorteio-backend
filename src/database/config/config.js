e0f284c75dc7ad07a0e690087beb764a0cf3a36e
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2022-09-15 14:28:30 -0300

    teste

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,12 +1,11 @@
-//require('dotenv').config({ path: __dirname+'/./../../../.env'});
-require('dotenv').config({ path: __dirname + '../../../.env'});
+require('dotenv').config({ path: __dirname+'/./../../../.env'});
 
 module.exports = {
     development: {
-        username: process.env.DATABASE_USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
+        username: process.env.DATABASE_USER || "b9a7891991700c",
+        password: process.env.PASSWORD || "92190dc9",
+        database: process.env.DATABASE || "heroku_cf29b3a264c0669",
+        host: process.env.HOST || "us-cdbr-east-06.cleardb.net",
         dialect: 'mysql',
         logging: false,
         define: {
