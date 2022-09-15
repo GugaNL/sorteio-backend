b2431ab13baeea98fde9cdcd2e3fffbd2d7901c4
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2022-09-15 11:35:49 -0300

    uncomment index routes imports

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const bodyPaser = require("body-parser");
 const port = process.env.PORT || 3000;
 
 const handle404Error = require("./src/middlewares/handle404Error");
-const usuarioRoute = require("./src/routers/usuario.route");
-const clienteRoute = require("./src/routers/cliente.route");
-const sorteioRoute = require("./src/routers/sorteio.route");
-const bilheteRoute = require("./src/routers/bilhete.route");
+// const usuarioRoute = require("./src/routers/usuario.route");
+// const clienteRoute = require("./src/routers/cliente.route");
+// const sorteioRoute = require("./src/routers/sorteio.route");
+// const bilheteRoute = require("./src/routers/bilhete.route");
 const handleError = require("./src/middlewares/handleError");
 
 app.get("/", function(req, res) {
