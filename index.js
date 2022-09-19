2af895394d10d8d0289b8badd6574fc0dfb0c954
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2022-09-19 11:14:43 -0300

    Added CORS

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require("express");
 const bodyPaser = require("body-parser");
 const app = express();
+const cors = require('cors');
+
 
 const handle404Error = require('./src/middlewares/handle404Error');
 
@@ -11,6 +13,7 @@ const sorteioRoute = require('./src/routers/sorteio.route');
 const bilheteRoute = require('./src/routers/bilhete.route');
 const handleError = require('./src/middlewares/handleError');
 
+app.use(cors());
 app.use(bodyPaser.urlencoded({ extended: true }));
 app.use(bodyPaser.json());
 app.use('/api/usuario', usuarioRoute);
